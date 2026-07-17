801c4efa58322b0d97080629d260608367ed96dd
Ultimate88 <alejandrogonzalez88x@gmail.com>
2026-07-17 15:34:30 +0200

    chore: configure postgresql-docker connection

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,32 @@
+using DotNetEnv;
+using backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+Env.Load("../.env");
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString =
+    $"Host=localhost;Port=5432;" +
+    $"Database={Environment.GetEnvironmentVariable("POSTGRES_DB")};" +
+    $"Username={Environment.GetEnvironmentVariable("POSTGRES_USER")};" +
+    $"Password={Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")}";
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins")
+    .Get<string[]>() ?? [];
 
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("Angular", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins(allowedOrigins)
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
