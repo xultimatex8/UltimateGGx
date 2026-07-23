1138a54fa62e451a62b9e4a25c0a17b955422bac
Alejandro Gonzalez <agmdeveloper@outlook.com>
2026-07-23 18:10:56 +0200

    feat: retrieve summoner info from riot api

---
+++ backend/Models/Riot/SummonerResponseDto.cs
@@ -0,0 +1,7 @@
+namespace backend.Models.Riot;
+
+public class SummonerResponseDto
+{
+    public int ProfileIconId { get; set; }
+    public int SummonerLevel { get; set; }
+}
--- backend/Services/RiotApiService.cs
+++ backend/Services/RiotApiService.cs
@@ -13,4 +13,24 @@ public class RiotApiService
         _regionalClient = httpClientFactory.CreateClient("RiotRegional");
     }
 
+    public async Task<AccountResponseDto> GetRiotAccountAsync(string username, string tag, CancellationToken ct = default)
+    {
+        AccountResponseDto? response = await _regionalClient.GetFromJsonAsync<AccountResponseDto>(
+            $"/riot/account/v1/accounts/by-riot-id/{username}/{tag}", ct);
+        return response ?? throw new Exception("Could not retrieve Riot Account Info");
+    }
+
+    public async Task<SummonerResponseDto> GetRiotSummonerAsync(string puuid, CancellationToken ct = default)
+    {
+        SummonerResponseDto? response = await _platformClient.GetFromJsonAsync<SummonerResponseDto>(
+            $"/lol/summoner/v4/summoners/by-puuid/{puuid}", ct);
+        return response ?? throw new Exception("Could not retrieve Riot Summoner Info");
+    }
+
+    public async Task<List<QueueResponseDto>> GetSummonerQueuesAsync(string puuid, CancellationToken ct = default)
+    {
+        List<QueueResponseDto>? response = await _platformClient.GetFromJsonAsync<List<QueueResponseDto>>(
+            $"/lol/league/v4/entries/by-puuid/{puuid}", ct);
+        return response ?? throw new Exception("Could not retrieve Riot Summoner Queues Info");
+    }
 }
