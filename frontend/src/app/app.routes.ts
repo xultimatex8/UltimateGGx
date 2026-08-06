375439d57da50555a33735ef717ac2e90f02e32e
Alejandro Gonzalez <agmdeveloper@outlook.com>
2026-08-06 17:05:29 +0200

    feat: add page not found screen

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home/home';
 import { SummonerProfile } from './summoner/profile/summoner-profile/summoner-profile';
 import { MatchTimeline } from './timeline/match-timeline/match-timeline';
 import { Terms } from './terms/terms';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'summoner/:username/:tag', component: SummonerProfile },
   { path: 'match/:matchId', component: MatchTimeline },
-  { path: 'terms', component: Terms}
+  { path: 'terms', component: Terms },
+  { path: '**', component: NotFound },
 ];
---
+++ frontend/src/app/pages/not-found/not-found.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [RouterLink],
+  templateUrl: './not-found.html',
+})
+export class NotFound {}
