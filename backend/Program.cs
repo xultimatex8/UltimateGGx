828f72bdfddb01dbd5a7fedb18e40138f7333d89
Alejandro Gonzalez <agmdeveloper@outlook.com>
2026-08-20 16:23:01 +0200

    feat: remove migrations from app start

--- backend/Program.cs
+++ backend/Program.cs
@@ -77,12 +77,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
-}
-
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
